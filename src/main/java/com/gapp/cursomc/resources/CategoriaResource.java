56947dea4b8443fa2e5014377a3622a1296cddda
Guilherme Freitas <guilherme.a.freitas@outlook.com>
2021-08-30 10:43:54 -0300

    Criacao classe de dominio - Categoria

---
+++ src/main/java/com/gapp/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,27 @@
+package com.gapp.cursomc.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gapp.cursomc.domain.Categoria;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+	
+	@GetMapping
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.addAll(Arrays.asList(cat1, cat2));
+		
+		return lista;
+	}
+}
