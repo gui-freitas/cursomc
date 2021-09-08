e82b7455b1423deb579d988432f9b124d3706b69
Guilherme Freitas <guilherme.a.freitas@outlook.com>
2021-09-08 16:21:16 -0300

    Criacao metodo DELETE - Categoria

--- src/main/java/com/gapp/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/gapp/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.gapp.cursomc.services.exceptions.DataIntegrityException;
 import com.gapp.cursomc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -19,5 +20,12 @@ public class ResourceExceptionHandler {
 		
 		StandardError standardError = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), Instant.now());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(standardError);
+	}
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrityException(DataIntegrityException e, HttpServletRequest request){
+		
+		StandardError standardError = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), Instant.now());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(standardError);
 	}
 }
