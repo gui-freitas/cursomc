40de7d4378062a2541307a8596248ca3c21b8093
Guilherme Freitas <guilherme.a.freitas@outlook.com>
2021-09-08 17:13:46 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/gapp/cursomc/services/CategoriaService.java
+++ src/main/java/com/gapp/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.gapp.cursomc.domain.Categoria;
+import com.gapp.cursomc.dto.CategoriaDTO;
 import com.gapp.cursomc.repositories.CategoriaRepository;
 import com.gapp.cursomc.services.exceptions.DataIntegrityException;
 import com.gapp.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer size, String order, String direction){
 		PageRequest pageRequest = PageRequest.of(page, size, Direction.valueOf(direction), order);
 		return categoriaRepository.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO obj) {
+		return new Categoria(obj.getId(), obj.getNome());
 	}
 }
