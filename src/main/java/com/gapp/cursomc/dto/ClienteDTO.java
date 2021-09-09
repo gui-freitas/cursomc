e6f8c16e9e85bdda972bd224a494054c5c678992
Guilherme Freitas <guilherme.a.freitas@outlook.com>
2021-09-09 17:17:01 -0300

    Validacao customizada de email nao repetido na atualizacao - Cliente

--- src/main/java/com/gapp/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/gapp/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.gapp.cursomc.domain.Cliente;
+import com.gapp.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
