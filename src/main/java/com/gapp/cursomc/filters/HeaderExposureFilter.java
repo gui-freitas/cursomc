bfd5fe741675489dda2b1f9db9c1f6f8722ca8af
Guilherme Freitas <guilherme.a.freitas@outlook.com>
2022-02-15 11:11:08 -0300

    Corrigindo exposicao do header location, e requisicoes post aos clientes

--- src/main/java/com/gapp/cursomc/filters/HeaderExposureFilter.java
+++ src/main/java/com/gapp/cursomc/filters/HeaderExposureFilter.java
@@ -18,7 +18,7 @@ public class HeaderExposureFilter implements Filter{
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		HttpServletResponse res = (HttpServletResponse) response;
-		res.addHeader("access-control-expose-headers", "Authorization");
+		res.addHeader("access-control-expose-headers", "Location");
 		chain.doFilter(request, response);
 	}
 }
