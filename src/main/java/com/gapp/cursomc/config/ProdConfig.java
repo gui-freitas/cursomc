0dd16c5214995b95c250c0f9122bc5219ad67392
Guilherme Freitas <guilherme.a.freitas@outlook.com>
2022-02-22 16:29:31 -0300

    Incluindo configuracao de prod

---
+++ src/main/java/com/gapp/cursomc/config/ProdConfig.java
@@ -0,0 +1,37 @@
+package com.gapp.cursomc.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.gapp.cursomc.services.DBService;
+import com.gapp.cursomc.services.EmailService;
+import com.gapp.cursomc.services.SmtpEmailService;
+
+@Configuration
+@Profile("prod")
+public class ProdConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabae() {
+		
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+		dbService.instantiateDatabase();
+		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
+	}
+}
